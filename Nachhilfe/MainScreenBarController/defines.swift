face1246ee19263d2ad0c5263a9c6e4bc0e819a5
Tschekalinskij <tsc@mbp498.speedport_w_724v_09011603_05_010>
2018-04-01 10:50:44 +0200

    Added labels and loading stroing to firebase

---
+++ Nachhilfe/MainScreenBarController/defines.swift
@@ -0,0 +1,9 @@
+//
+//  defines.swift
+//  Nachhilfe
+//
+//  Created by Tschekalinskij, Alexander on 28.03.18.
+//  Copyright © 2018 Tschekalinskij, Alexander. All rights reserved.
+//
+
+import Foundation
