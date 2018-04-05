0dd8f1cf20d674cd4c2554be3465838c267f11d8
Tschekalinskij <tsc@mbp498.iis.fhg.de>
2018-04-05 15:48:26 +0200

    Added settings page with sliders

---
+++ Nachhilfe/MainScreenBarController/SettingsSliders.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsSliders.swift
+//  Nachhilfe
+//
+//  Created by Tschekalinskij, Alexander on 04.04.18.
+//  Copyright © 2018 Tschekalinskij, Alexander. All rights reserved.
+//
+
+import Foundation
